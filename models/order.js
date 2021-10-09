14393ad08b1df8320b3220d466344ac9b55a577b
kosorio2 <oso16001@byui.edu>
2021-10-09 12:12:37 -0600

    added the cart and the post order controller

---
+++ models/order.js
@@ -0,0 +1,27 @@
+//This will store data in the orders collection 
+
+const mongoose = require('mongoose'); 
+
+const Schema = mongoose.Schema; 
+
+const orderSchema = new Schema({
+    products: [
+    {
+        product: { type: Object, required: true },
+        quantity: { type: Number, required: true }
+    }
+    ],
+    user: {
+        name: {
+            type: String, 
+            required: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            required: true, 
+            ref: 'User' //This refers to the User model 
+        }
+    }
+}); 
+
+module.exports = mongoose.model('Order', orderSchema); 
